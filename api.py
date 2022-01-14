c05d4090ca6e3bc744060f4e62e7f4313326c856
EdderLp <roy@loadingplay.io>
2022-01-14 14:48:26 +0000

    fix #active/api

--- api.py
+++ api.py
@@ -6,11 +6,12 @@ from app.convert_sap_document import ConvertSapDocument
 api = FastAPI()
 
 
-@api.post("/v1/reception")
+@api.post("/v1/generate_document")
 def post_sap(data: dict):
     logging.info(f"Data: {data}")
     json_data = ConvertSapDocument(data)
     data_converted = json_data.join_json_sap()
     logging.info(f"Data transformed: {data_converted}")
-    return data_converted
+    request_document = json_data.send_sap(data_converted)
+    return request_document
 
