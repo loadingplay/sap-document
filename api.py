f75bf66c1468581861a86cf4d2bc5118ecbc5df3
EdderLp <roy@loadingplay.io>
2022-02-17 20:23:43 +0000

    fix #fix/test_sap

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import logging
 from fastapi import FastAPI
 from app.convert_sap_document import ConvertSapDocument
+from fastapi import HTTPException
+from app.exceptions import JoinJsonSapError
 
 
 logging.getLogger().setLevel(logging.INFO)
@@ -9,12 +11,22 @@ api = FastAPI()
 
 @api.post("/v1/generate_document")
 def post_sap(data: dict):
-    order = data["order"]["id"]
-    logging.info(f"order arrived: {order} Data: {data}")
+    order_lp = data["order"].get("id", "")
+    logging.info(f"order arrived: {order_lp} Data: {data}")
     json_data = ConvertSapDocument(data)
-    data_converted = json_data.join_json_sap()
-    logging.info(f"Data transformed order_id: {order} data: {data_converted}")
+    try:
+        data_converted = json_data.join_json_sap()
+    except JoinJsonSapError as ex:
+        logging.error(f"Error on data conversion: {ex}")
+        raise HTTPException(
+            400,
+            "Error on data conversion"
+        )
+
+    logging.info(
+        f"Data transformed order_id: {order_lp} data: {data_converted}")
     request_document = json_data.validate_article_in_sap()
-    logging.info(f"Data response of sap order: {order} response: {request_document}")
+    logging.info(
+        f"Data response of sap order: {order_lp} response: {request_document}")
     return request_document
 
