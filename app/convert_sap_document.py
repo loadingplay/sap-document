bba140f4af7a06255d918dac769436131fad6c6f
Manu <manuel@loadingplay.io>
2022-06-16 12:48:38 -0400

    fix error with rut #fix/rut

--- app/convert_sap_document.py
+++ app/convert_sap_document.py
@@ -28,6 +28,11 @@ class ConvertSapDocument():
         order = self.__data["order"]
 
         FederalTaxID = self.__data["order"]["customer"]["rut"]
+        if "-" not in FederalTaxID:
+            rut = FederalTaxID[:-1]
+            digito_verificador = FederalTaxID[-1]
+            FederalTaxID = rut + "-" + digito_verificador
+
         address = order["customer"]["address"]
         city = order["customer"]["country"]
         street = address[:99]
