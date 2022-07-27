332a2bf625df2cc19c0a387a5a06bc4933e68806
EdderLp <roy@loadingplay.io>
2022-07-27 15:00:49 +0000

    sap/creditnote

--- api.py
+++ api.py
@@ -12,19 +12,12 @@ api = FastAPI()
 
 @api.post("/v1/generate_document")
 def post_sap(data: dict):
-    request_document = {}
     order_lp = data["order"].get("id", "")
     logging.info(f"order arrived: {order_lp} Data: {data}")
     try:
-        if data["sap_json"]["config"].get("type_document", "") == "39":
-            json_data = ConvertSapDocument(data)
-            data_converted = json_data.join_json_sap()
-            request_document = json_data.validate_article_in_sap()
-        else:
-            json_data = SapCreditNote(data)
-            data_converted = json_data.build_credit_note()
-            # enviar JSON de credit note a Sap
-            request_document = json_data.send_credit_note()
+        json_data = ConvertSapDocument(data)
+        data_converted = json_data.join_json_sap()
+        request_document = json_data.validate_article_in_sap()
     except JoinJsonSapError as ex:
         logging.error(f"Error on data conversion: {ex}")
         raise HTTPException(
@@ -37,4 +30,27 @@ def post_sap(data: dict):
     # request_document = json_data.validate_article_in_sap()
     logging.info(
         f"Data response of sap order: {order_lp} response: {request_document}")
+    return request_document
+
+@api.post("/v1/generate_document/credit_note")
+def post_sap_credit_note(data: dict):
+    order_lp = data["order"].get("id", "")
+    logging.info(f"order for credit note arrived: {order_lp} Data: {data}")
+    try:
+        json_data = SapCreditNote(data)
+        data_converted = json_data.build_credit_note()
+        # enviar JSON de credit note a Sap
+        request_document = json_data.send_credit_note()
+    except JoinJsonSapError as ex:
+        logging.error(f"Error on data conversion credit_note: {ex}")
+        raise HTTPException(
+            400,
+            "Error on data conversion"
+        )
+
+    logging.info(
+        f"Data transformed credit_note order_id: {order_lp} data: {data_converted}")
+    # request_document = json_data.validate_article_in_sap()
+    logging.info(
+        f"Data response of sap credit_note order: {order_lp} response: {request_document}")
     return request_document
